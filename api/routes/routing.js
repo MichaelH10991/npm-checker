ff5fe0d1c620452b9dea237482751b1662f01c87
Michael Humphries <michaelhumphries1997@gmail.com>
2019-09-19 14:19:13 +0100

    updating

--- api/routes/routing.js
+++ api/routes/routing.js
@@ -3,15 +3,6 @@ const router = express.Router()
 const controller = require("./data.controller")
 const app = express()
 
-// serve the frontend by rendering the template
-app.get("/", (req, res) => {
-    res.render("index", { title: "Home Page" })
-})
-
-app.get("/about", (req, res) => {
-    res.render("about", { title: "About Page" })
-})
-
 router.get("/registry/:name", controller.reg)
 
 module.exports = router
--- server.js
+++ server.js
@@ -14,10 +14,19 @@ app.engine('hbs', hbs({
     partialsDir: path.join(__dirname, 'views/partials')
 }))
 
-app.set('view engine', 'handlebars')
+app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// serve the frontend by rendering the template
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home Page" })
+})
+
+app.get("/about", (req, res) => {
+    res.render("about", { title: "About Page" })
+})
 
 // Routing middleware to interface with npm api
 app.use("/api", require("./api/routes/routing"))
